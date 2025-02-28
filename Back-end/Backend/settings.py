3018293b49f148b996542e22c7437ced5d963c42
sekibaalamark <markmajje256@gmail.com>
2025-02-28 10:39:27 +0300

    Changed my database from sqlite3 to Postgres

--- Back-end/Backend/settings.py
+++ Back-end/Backend/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'Backend.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'mark_majje',
+        'USER':'mark_majje',
+        'PASSWORD':'mark',
     }
 }
 
