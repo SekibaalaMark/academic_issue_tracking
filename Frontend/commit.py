ea2fa4faf10ca1418dd109d2db2b8cb80c42334b
Ssegie <158040669+Ssegie@users.noreply.github.com>
2025-03-13 15:11:32 +0300

    'updates'

--- Frontend/commit.py
+++ Frontend/commit.py
@@ -6,7 +6,7 @@ def commit():
     os.system("git add .")
     os.system("git commit -m 'updates'")
     os.system("git push origin main")
-    time.sleep(3600)
+    time.sleep(2)
 
 if __name__ == '__main__':
     commit()
