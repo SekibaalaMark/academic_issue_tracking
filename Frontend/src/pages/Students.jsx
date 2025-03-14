dc1b1cdf7c264b5cf4216fce171bf0bc176e9ba1
Ssegie <158040669+Ssegie@users.noreply.github.com>
2025-03-14 23:03:41 +0300

    'updates'

--- Frontend/src/pages/Students.jsx
+++ Frontend/src/pages/Students.jsx
@@ -2,6 +2,17 @@ import React,{useState,useEffect} from "react";
 import axios from 'axios';
 import styled from "styled-components"
 
+const Students = () => {
+  const [issues, setIssues] = useState([]);
+  const [notifications, setNotifications] = useState([]);
+  const [formData, setFormData] = useState({
+    courseCode:"",
+    issueType:"missing marks",
+    description:"",
+  })
+  const [alertMessage, setAlertMessage] = useState("");
+  
+
 
 function Students() {
   return <h1>Welcome to the Academic Tracking System</h1>;
