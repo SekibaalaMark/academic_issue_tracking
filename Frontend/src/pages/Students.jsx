0180710c6c46a806959944d21877d63e60a6e4e3
Ssegie <158040669+Ssegie@users.noreply.github.com>
2025-03-14 22:51:08 +0300

    'updates'

--- Frontend/src/pages/Students.jsx
+++ Frontend/src/pages/Students.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import axios from 'axios';
+import styled from "styled-components"
+
 
 function Students() {
   return <h1>Welcome to the Academic Tracking System</h1>;
