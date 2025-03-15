986adfe75508418e001c3d86a6293ca549717b31
Ssegie <158040669+Ssegie@users.noreply.github.com>
2025-03-15 16:40:30 +0300

    'updates'

--- Frontend/commit.py
+++ Frontend/commit.py
@@ -1,12 +1,13 @@
 import os
 import time
 
-
 def commit():
+    
+    os.system("git checkout segie")
     while True:
         os.system("git add .")
         os.system("git commit -m 'updates'")
-        os.system("git push origin main")
-        time.sleep(3600)
+        os.system("git push origin segie")
+        time.sleep(3600)  # waits for 1 hour before next commit
 
 commit()
