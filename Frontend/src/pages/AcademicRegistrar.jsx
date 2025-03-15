2e94658d309b45a2cb908ef82577f947ef758de5
Ssegie <158040669+Ssegie@users.noreply.github.com>
2025-03-16 01:46:35 +0300

    'updates'

--- Frontend/src/pages/AcademicRegistrar.jsx
+++ Frontend/src/pages/AcademicRegistrar.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import styled from "styled-components";
 
 function AcademicRegistrar() {
   return <h1>Welcome to the Academic Tracking System</h1>;
