cae411ff33ef39a05cb29cb60a9e100023054a8e
Ssegie <158040669+Ssegie@users.noreply.github.com>
2025-04-03 19:07:28 +0300

    Update App1

--- Frontend/src/App1.jsx
+++ Frontend/src/App1.jsx
@@ -12,7 +12,7 @@ import ForgotPassword from "./features/authentication/ForgotPassword.jsx";
 import  Logout from "./components/Logout"; // Import  Logout from "./StudentComponents/Logout";
 import { Container } from "@mui/material";        
 import Students from "./pages/Students.jsx";
-import "./App.css"; // Import your CSS file for styling
+import "./App.css";   
 import AcademicRegistrar from "./pages/AcademicRegistrar.jsx";
 // import LecturerDashboard from "./pages/LecturerDashboard.jsx"; // Added LecturerDashboard
 
@@ -30,13 +30,13 @@ const ProtectedLayout = () => {
   if (isAuthenticated) {
     switch (userRole) {
       case "student":
-        return <Navigate to="/student-dashboard" replace />;
+        return <Navigate to="/Students" replace />;
       case "lecturer":
         return <Navigate to="/lecturers" replace />;
       case "academicregistrar":
         return <Navigate to="/AcademicRegistrar" replace />;
       default:
-        return <Navigate to="/dashboard" replace />;
+        return <Navigate to="/coverpage" replace />;
     }
   }
 
