89d2afa1e979ae2213e4afb5e8701b8c89302a8c
sekibaalamark <markmajje256@gmail.com>
2025-04-03 15:37:31 +0300

    organizing branch to host to Herocu

--- Frontend/src/ui/Input.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Input = styled.input`
-  padding: 1.2rem;
-  border: 1px solid #333;
-  border-radius: 5px;
-`;
-
-export default Input;
--- README.md
+++
@@ -1,1 +0,0 @@
-The AITS is a system that makes resolving the student issues with tracking
