0fe42a145654472dadcf64ac8f76793a4e327cfe
sekibaalamark <markmajje256@gmail.com>
2025-04-03 09:41:34 +0300

    removed the duplicate URLs

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('',include('api.urls')),
+    #path('',include('api.urls')),
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
       ]
