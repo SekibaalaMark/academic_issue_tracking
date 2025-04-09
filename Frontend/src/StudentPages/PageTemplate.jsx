e7e8901002fcf4ad82435ebafcc8397bb82ed116
Donata256 <niyigabadonatah@gmail.com>
2025-04-09 20:04:38 +0300

    Merge donatah into main

--- Frontend/src/StudentPages/PageTemplate.jsx
+++
@@ -1,5 +0,0 @@
-function PagesTemplate() {
-  return <div>hello</div>;
-}
-
-export default PagesTemplate;
--- Frontend/src/ui/Button.jsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const Button = styled.button`
-  padding: 1rem 1.4rem;
-`;
-
-export default Button;
